0d2a329376dadcba2b43660f3a4732cf4138c0e1
francoS18 <francosebastian.garrido@alumnos.ulagos.cl>
2023-05-16 19:33:03 -0400

    subida

--- Reto_en_clases_04.py
+++ Reto_en_clases_04.py
@@ -1,10 +1,12 @@
-paciente=[]
+pacientes = []
 for i in range(3):
     nombre=input("Ingrese el nombre del paciente {}: ".format(i+1))
-    peso=input("Ingrese el peso del paciente {}: ".format(i+1))
-    estatura=float(input("Ingrese la estatura del paciente {} ".format(i+1)))
-    edad=input("Ingrese la edad del paciente {}".format(i+1))
-    while not edad.isdigit() and int(edad)<0:
-        print("Edad invalida. Intente nuevamente")
-        edad= input("Ingrese la edad del paciente {} :".format(i+1))
-        
+    peso=float(input("Ingrese el peso del paciente {} (kg): ".format(i+1)))
+    estatura=float(input("Ingrese la estatura del paciente {} (m): ".format(i+1)))
+    edad=input("Ingrese la edad del paciente: ")
+    while not edad.isdigit() and int(edad) < 0:
+        print("Edad invalida. Intente nuevamente.")
+        edad=input("Ingrese la edad del paciente {}: ".format(i+1))
+    pacientes.append((nombre,peso,estatura,edad))
+
+print(pacientes)
