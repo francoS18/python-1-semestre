468d034d1e6a22b3fbfce9d691c63c729512a787
francoS18 <francosebastian.garrido@alumnos.ulagos.cl>
2023-05-20 15:11:03 -0400

    edit

--- Reto_en_clases_04.py
+++ Reto_en_clases_04.py
@@ -4,7 +4,7 @@ for i in range(3):
     peso=float(input("Ingrese el peso del paciente {} (kg): ".format(i+1)))
     estatura=float(input("Ingrese la estatura del paciente {} (m): ".format(i+1)))
     edad=input("Ingrese la edad del paciente: ")
-    while not edad.isdigit() and int(edad) < 0:
+    while not edad.isdigit() or int(edad) < 0:
         print("Edad invalida. Intente nuevamente.")
         edad=input("Ingrese la edad del paciente {}: ".format(i+1))
     pacientes.append((nombre,peso,estatura,edad))
