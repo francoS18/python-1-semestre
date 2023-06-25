d6da04d71e95b11ba215dfb62cf07d1a2f23c52c
francoS18 <francosebastian.garrido@alumnos.ulagos.cl>
2023-06-25 16:47:08 -0400

    Actualizacion 25/06/2023

--- Parcial 2/Ejercicio_02.py
+++ Parcial 2/Ejercicio_02.py
@@ -1,11 +1,49 @@
-num=1
-lista_de_numeros=[num]
-print(lista_de_numeros)
-for i in range(10):
-    num=num+1
-    lista_de_numeros.append(num)
-    lista_de_numeros.append(num+1)
-    lista_de_numeros.remove(lista_de_numeros[0])
-    #lista_de_numeros.insert()
-    print(lista_de_numeros)
+while True:
+    for i in range(1):
+        print("__________1__________")
+    break
 
+while True:
+    for i in range(1):
+        print("_________232_________")
+    break
+
+while True:
+    for i in range(1):
+        print("________34543________")
+    break
+
+while True:
+    for i in range(1):
+        print("_______4567654_______")
+    break
+
+while True:
+    for i in range(1):
+        print("______567898765______")
+    break
+
+while True:
+    for i in range(1):
+        print("_____67890109876_____")
+    break
+
+while True:
+    for i in range(1):
+        print("____7890123210987____")
+    break
+
+while True:
+    for i in range(1):
+        print("___890123454321098___")
+    break
+
+while True:
+    for i in range(1):
+        print("_9012345678765432109_")
+    break
+
+while True:
+    for i in range(1):
+        print("012345678909876543210")
+    break
--- Retos/Reto_06.py
+++
@@ -1,2 +0,0 @@
-suma=[10,12,14,16,18,20,22,24,26,28,30]
-print(f"La suma de los números del rango 10 al 30, incrementando de 2 en 2 es: {sum(suma)}")
